96d86753d503c65d737b1c21999f61eb931eabd9
uditbaliyan <130930448+uditbaliyan@users.noreply.github.com>
2024-07-26 20:46:40 +0530

    Update settings.py

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+cze%0+*oz67ke)+)&6zncb_^rkr0=o!cgqlx$br3(@#-u+2^1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
